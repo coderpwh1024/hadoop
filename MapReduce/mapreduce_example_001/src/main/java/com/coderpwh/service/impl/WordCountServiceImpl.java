17688e6d618ea41b7efd4793e4ad95294bd799c3
pengwenhao <pengwenhao@aliyun.com>
2024-10-31 11:46:39 +0800

    fix:update mapreduce

--- MapReduce/mapreduce_example_001/src/main/java/com/coderpwh/service/impl/WordCountServiceImpl.java
+++ MapReduce/mapreduce_example_001/src/main/java/com/coderpwh/service/impl/WordCountServiceImpl.java
@@ -1,8 +1,14 @@
 package com.coderpwh.service.impl;
 
 import com.coderpwh.service.WordCountService;
+import com.coderpwh.service.mapreduce.IntSumReducer;
+import com.coderpwh.service.mapreduce.TokenizerMapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.GenericOptionsParser;
 import org.springframework.stereotype.Service;
-import org.apache.hadoop.mapreduce.Mapper;
 
 /**
  * @author coderpwh
@@ -17,6 +23,29 @@ public class WordCountServiceImpl implements WordCountService {
      */
     @Override
     public String getWordCountInfo() {
+        try {
+            String[] arr = new String[10];
+            Configuration conf = new Configuration();
+            String[] otherArgs = (new GenericOptionsParser(conf, arr)).getRemainingArgs();
+            if (otherArgs.length < 2) {
+                System.err.println("Usage: wordcount <in> [<in>...] <out>");
+                System.exit(2);
+            }
+
+            Job job = Job.getInstance(conf, "word count");
+            job.setJarByClass(WordCountServiceImpl.class);
+            job.setMapperClass(TokenizerMapper.class);
+            job.setCombinerClass(IntSumReducer.class);
+            job.setReducerClass(IntSumReducer.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(IntWritable.class);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 
