f09313eb85b9a1be79dff97ff543eb9dc4387914
pengwenhao <pengwenhao@aliyun.com>
2024-10-31 10:12:45 +0800

    feat:init mapreduce

---
+++ MapReduce/mapreduce_example_001/src/main/java/com/coderpwh/MapReduceApplication.java
@@ -0,0 +1,19 @@
+
+package com.coderpwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author coderpwh
+ */
+
+@SpringBootApplication
+public class MapReduceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapReduceApplication.class, args);
+    }
+
+
+}
