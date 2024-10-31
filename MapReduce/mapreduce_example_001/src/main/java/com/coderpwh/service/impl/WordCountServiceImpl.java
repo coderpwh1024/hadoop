2a88ae1a415461559bbc206db6c33d1b858c563a
pengwenhao <pengwenhao@aliyun.com>
2024-10-31 13:40:29 +0800

    fix:WordCountService

--- MapReduce/mapreduce_example_001/src/main/java/com/coderpwh/service/impl/WordCountServiceImpl.java
+++ MapReduce/mapreduce_example_001/src/main/java/com/coderpwh/service/impl/WordCountServiceImpl.java
@@ -6,9 +6,12 @@ import com.coderpwh.service.mapreduce.TokenizerMapper;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.springframework.stereotype.Service;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 /**
  * @author coderpwh
@@ -39,14 +42,15 @@ public class WordCountServiceImpl implements WordCountService {
             job.setReducerClass(IntSumReducer.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(IntWritable.class);
-
-
+            for (int i = 0; i < otherArgs.length - 1; i++) {
+                FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
+            }
+            FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
+            System.exit(job.waitForCompletion(true) ? 0 : 1);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-        return null;
+        return "success";
     }
 
 
